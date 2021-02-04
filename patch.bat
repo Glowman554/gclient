8391860204afa572b81586dc9f3135903e45b3b3
janick voss <vossjanick@gmail.com>
2021-02-04 09:39:25 +0100

    new patches

--- patch.bat
+++ patch.bat
@@ -5,7 +5,9 @@ git init
 git add *
 git commit -m "init"
 
-git am < ..\patches\0001-featured-servers.patch
-git am < ..\patches\0002-discord-rp.patch
+git am < ..\patches\0001-optifine.patch
+git am < ..\patches\0002-optifine-assets.patch
+git am < ..\patches\0003-featured-server.patch
+git am < ..\patches\0004-discord-rp.patch
 
 cd ..
