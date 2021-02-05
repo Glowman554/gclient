cab69f8ef643bbd38163baee4ee5aaa6edc7e935
Glowman554 <55138266+Glowman554@users.noreply.github.com>
2021-02-05 08:20:06 +0100

    Add files via upload

--- patch.bat
+++ patch.bat
@@ -9,5 +9,6 @@ git am < ..\patches\0001-optifine.patch
 git am < ..\patches\0002-optifine-assets.patch
 git am < ..\patches\0003-featured-server.patch
 git am < ..\patches\0004-discord-rp.patch
+git am < ..\patches\0005-event-manager.patch
 
 cd ..
