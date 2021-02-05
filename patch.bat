6465dcddc287191fc70d135ab5740a16f1abaf2c
Glowman554 <55138266+Glowman554@users.noreply.github.com>
2021-02-05 14:34:03 +0100

    Add files via upload

--- patch.bat
+++ patch.bat
@@ -11,5 +11,6 @@ git am < ..\patches\0003-featured-server.patch
 git am < ..\patches\0004-discord-rp.patch
 git am < ..\patches\0005-event-manager.patch
 git am < ..\patches\0006-hud.patch
+git am < ..\patches\0007-filemanager.patch
 
 cd ..
